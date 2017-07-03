aad50a3d0ac668e1355d4e6b2219b6e2d8d583b3
Hack Reactor Student <student@hackreactor.com>
2017-07-03 12:31:48 -0700

    " Recursion first part, stringifyJSON"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,44 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof(obj) === 'string') {
+    return '"' + obj + '"';
+  }
+  if (obj === null) {
+    return 'null';
+  }
+  if (obj === undefined || typeof(obj) === 'function') {
+    return;
+  } 
+  if (Array.isArray(obj)) {
+    var output = [];
+    if (obj.length === 0) {
+      return '[]';
+    } 
+    for (var i = 0; i < obj.length; i++) {
+      output.push(stringifyJSON(obj[i])); 
+    }
+    return '[' + output.join(',') + ']';
+  }
+  if (obj instanceof Object) {
+    var objoutput = [];
+    var key = Object.keys(obj);
+    if (key.length === 0) {
+      return '{}';
+    } 
+    for (var keys in obj) {
+      if (typeof obj[keys] === 'function' || keys === undefined || typeof keys === 'function' || obj[keys] === undefined) {
+        continue;
+      } else {
+        objoutput.push(stringifyJSON(keys) + ':' + stringifyJSON(obj[keys]));
+      } 
+    }
+    return '{' + objoutput.join(',') + '}';
+  }  
+  return obj.toString();
+
 };
+
+
+
+
