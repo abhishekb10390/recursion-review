a26197c79377e86042afd4906e8a84bf7302f74d
Hack Reactor Student <student@hackreactor.com>
2017-07-03 19:05:29 -0700

    ElementsByClassNameSpec

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,18 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className, node) {
   // your code here
+  node = node || document.body;
+  var elements = [];
+  if (node.classList) {
+    if (node.classList.contains(className)) {
+      elements.push(node);
+    }
+  }
+  for (var i = 0; i < node.childNodes.length; i++) {
+    var matches = getElementsByClassName(className, node.childNodes[i]);
+    elements = elements.concat(matches);
+  }
+  return elements;
 };
--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,34 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  //function for number , string, boolean, null, undefined, object, array 
+  if (json[0] === '{') {
+    //we know its an obect
+    var output = {};  
+    for (var i = 2; i < json.length; i++) {
+      var key = '';
+      var value = '';
+      if (json[i] === '"') {
+        var j = i + 3;
+        while ((json[j] !== '"' || json[j] !== '}') && j < json.length) {
+          value += json[j];
+          j++;
+        }
+console.log(key, value);
+        output[eval(key)] = parseJSON(value);  
+      }
+      key = key + json[i];
+console.log(key)
+    }
+    return output;
+  } else if (json[0] === '[') {
+    //its an array
+  } else {
+    return primitiveParser(json);  
+  } 
+    
+  var primitiveParser = function(value) {
+    return eval(value);
+  };
 };
+
